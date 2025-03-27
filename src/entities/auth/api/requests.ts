e353cd6a7f57fbdbb0a37912dd85c247bea8e9cc
ivanfed <dev.fediciv@gmail.com>
2025-03-27 08:04:46 +0100

    feat: test linter

--- src/entities/auth/api/requests.ts
+++ src/entities/auth/api/requests.ts
@@ -1,5 +1,5 @@
 import { http } from '@core/shared/libs/http';
 
-export async function register(): Promise<void> {
+export async function register(): Promise<string> {
   return await http('abracadabra test');
 }
