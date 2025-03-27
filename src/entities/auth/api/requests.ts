4e4c6e8bbfa39254c39e32326849216af70add0e
ivanfed <dev.fediciv@gmail.com>
2025-03-27 09:11:50 +0100

    feat: test new github app tokens

--- src/entities/auth/api/requests.ts
+++ src/entities/auth/api/requests.ts
@@ -1,5 +1,5 @@
 import { http } from '@core/shared/libs/http';
 
 export async function register(): Promise<string> {
-  return await http('abracadabra test');
+  return await http('abracadabra test 2');
 }
