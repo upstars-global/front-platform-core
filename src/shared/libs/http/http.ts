348ad4751fc6ff41d5ffb97cb8e65f7a5affcc23
Ivan Fed <41876762+i-van-develop@users.noreply.github.com>
2025-04-09 13:38:05 +0200

    FP 2948 - add http lib, add auth features (#2)
    * docs[FP-2948]: fix repo name in docs
    
    * chore[FP-2948]: add verbatimModuleSyntax, remove composite in tsconfig
    
    * chore[FP-2948]: remove artefact tsconfig.tsbuildinfo
    
    * chore[FP-2948]: remove initial code
    
    * feat[FP-2948]: add http lib
    - add http controller
    - add backend api adapters
    - add mitt and uuid dependencies
    
    * docs[FP-2948]: fix semantic-release.md docs
    
    * fix[FP-2948]: use the same versions as main project
    
    * fix[FP-2948]: add tsconfig.tsbuildinfo to ignore, return composite option to tsconfig
    
    * fix[FP-2948]: try tsconfig without composite: true

--- src/shared/libs/http/http.ts
+++ src/shared/libs/http/http.ts
@@ -1,3 +1,136 @@
-export function http<T>(data: T): Promise<T> {
-  return Promise.resolve(data);
+import type { HTTPOptions } from './types';
+import { getHttpConfig } from './config';
+import { exponentialBackoff, concatBaseUrl, checkTimeoutExceed, createHttpAbortController } from './helpers';
+import {
+  type JsonHttpError,
+  JsonHttpJsonParseError,
+  JsonHttpServerError,
+  JsonHttpTimeoutError,
+  JsonHttpUnknownError,
+} from './errors';
+
+export function jsonHttp<R = unknown>(url: string, request?: RequestInit, options: HTTPOptions = {}): Promise<R> {
+  const { timeout, retryCount, headersDecorator, baseUrl } = getHttpConfig();
+
+  const requestUrl = concatBaseUrl(options?.baseURL || baseUrl, url);
+
+  const headers = new Headers(request?.headers);
+  headers.set('X-Requested-With', 'XMLHttpRequest');
+  headers.set('Accept', 'application/json');
+  if (request?.body) {
+    headers.set('Content-Type', 'application/json');
+  }
+  headersDecorator(headers);
+
+  return new Promise((resolve, reject: (error: JsonHttpError) => void) => {
+    function requestAttempt(attempt = 1) {
+      const { signal } = createHttpAbortController(timeout);
+
+      fetch(requestUrl, {
+        ...request,
+        signal,
+        headers,
+      })
+        .then((response) => {
+          if (response.ok) {
+            response
+              .json()
+              .then((data: R) => {
+                resolve(data);
+              })
+              .catch(() => {
+                response
+                  .text()
+                  .then((text) => {
+                    reject(
+                      new JsonHttpJsonParseError({
+                        textOutput: text,
+                        url: requestUrl,
+                        method: request?.method || 'GET',
+                      }),
+                    );
+                  })
+                  .catch(() => {
+                    reject(
+                      new JsonHttpJsonParseError({
+                        textOutput: '',
+                        url: requestUrl,
+                        method: request?.method || 'GET',
+                      }),
+                    );
+                  });
+              });
+          } else {
+            response
+              .json()
+              .then((data: unknown) => {
+                reject(
+                  new JsonHttpServerError({
+                    status: response.status,
+                    statusText: response.statusText,
+                    data,
+                    url: requestUrl,
+                    method: request?.method || 'GET',
+                  }),
+                );
+              })
+              .catch(() => {
+                response
+                  .text()
+                  .then((data: string) => {
+                    reject(
+                      new JsonHttpServerError({
+                        status: response.status,
+                        statusText: response.statusText,
+                        data,
+                        url: requestUrl,
+                        method: request?.method || 'GET',
+                      }),
+                    );
+                  })
+                  .catch(() => {
+                    reject(
+                      new JsonHttpServerError({
+                        status: response.status,
+                        statusText: response.statusText,
+                        url: requestUrl,
+                        method: request?.method || 'GET',
+                      }),
+                    );
+                  });
+              });
+          }
+        })
+        .catch((error: unknown) => {
+          const isTimeoutExceed = checkTimeoutExceed(signal);
+          const isOnLine = typeof window === 'undefined' || window.navigator.onLine;
+
+          if (isTimeoutExceed || (!isOnLine && attempt < retryCount)) {
+            exponentialBackoff(attempt)
+              .then(() => {
+                requestAttempt(attempt + 1);
+              })
+              .catch((retryError: unknown) => {
+                reject(
+                  new JsonHttpTimeoutError({
+                    error: retryError,
+                    url: requestUrl,
+                    method: request?.method || 'GET',
+                  }),
+                );
+              });
+          } else {
+            reject(
+              new JsonHttpUnknownError({
+                error,
+                url: requestUrl,
+                method: request?.method || 'GET',
+              }),
+            );
+          }
+        });
+    }
+
+    requestAttempt();
+  });
 }
--- src/shared/libs/http/index.ts
+++ src/shared/libs/http/index.ts
@@ -1,1 +1,5 @@
 export * from './http';
+export * from './types';
+export * from './errors';
+export { configHttp } from './config';
+export * from './adapters';
