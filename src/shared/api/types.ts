f54c4469a4be8704f3538f5637e5f2228eb089f4
ivanfed <dev.fediciv@gmail.com>
2025-07-01 10:38:44 +0200

    feat(FP-3157): add global types

---
+++ src/shared/api/types.ts
@@ -0,0 +1,30 @@
+export enum Currency {
+  AUD = 'AUD',
+  NZD = 'NZD',
+  CAD = 'CAD',
+  EUR = 'EUR',
+  USD = 'USD',
+  INR = 'INR',
+  BRL = 'BRL',
+}
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+export enum Locale {
+  ENGLISH = 'en',
+  CANADA_ENGLISH = 'en-CA',
+  SECOND_CANADA_ENGLISH = 'km', // not an error. Agreement between teams to define second en-CA locale as km
+  AUSTRALIA_ENGLISH = 'en-AU',
+  NEW_ZEALAND_ENGLISH = 'en-NZ',
+  INDIAN_ENGLISH = 'en-IN',
+  FRENCH = 'fr',
+  CANADA_FRENCH = 'fr-CA',
+  PORTUGUESE = 'pt',
+  BRASILIA_PORTUGUESE = 'pt-BR',
+  SPANISH = 'es',
+  GERMAN = 'de',
+  ITALIAN = 'it',
+}
