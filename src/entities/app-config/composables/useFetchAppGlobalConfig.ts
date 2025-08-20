d87ff1242f804dce30df125db8834feadcb5f46b
Daniil Balianytsia <d.balianytsia@upstars.com>
2025-08-20 13:47:09 +0300

    FP-3164: refactored loadAppGlobalConfig to use promiseMemo for improved caching

--- src/entities/app-config/composables/useFetchAppGlobalConfig.ts
+++ src/entities/app-config/composables/useFetchAppGlobalConfig.ts
@@ -1,22 +1,23 @@
 import { configAPI } from '../api';
-import { type AppGlobalConfig } from '../types';
 import { promiseMemo } from '../../../shared/helpers/promise';
 import { useAppGlobalConfigStore } from '../store';
 import type { Pinia } from 'pinia';
 
-export const loadGlobalConfig = promiseMemo(async () => {
-  return await configAPI.loadAppGlobalConfig();
-});
-
 export function useFetchAppGlobalConfig(pinia?: Pinia) {
   const appGlobalConfigStore = useAppGlobalConfigStore(pinia);
 
-  async function loadAppGlobalConfig(): Promise<AppGlobalConfig | undefined> {
-    if (!appGlobalConfigStore.isLoaded) {
-      return await loadGlobalConfig();
-    }
-    return appGlobalConfigStore.globalConfig;
-  }
+  const loadAppGlobalConfig = promiseMemo(
+    async () => {
+      if (!appGlobalConfigStore.isLoaded) {
+        return await configAPI.loadAppGlobalConfig();
+      }
+
+      return appGlobalConfigStore.globalConfig;
+    },
+    {
+      key: 'loadAppGlobalConfig',
+    },
+  );
 
   return {
     loadAppGlobalConfig,
