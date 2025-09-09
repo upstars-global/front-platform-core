f28ca397bbb97c3bcba51bbd885ec55975b97908
Daniil Balianytsia <d.balianytsia@upstars.com>
2025-09-09 15:29:30 +0300

    refactor(FP-3163): integrate configEnvironment for dynamic environment settings

--- src/entities/environment/store/useEnvironmentStore.ts
+++ src/entities/environment/store/useEnvironmentStore.ts
@@ -1,20 +1,17 @@
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import { EnvironmentType } from '../types';
-
-const DEVELOPMENT_ENV = EnvironmentType.DEVELOPMENT;
-const STAGING_ENV = EnvironmentType.STAGING;
-const PRODUCTION_ENV = EnvironmentType.PRODUCTION;
+import { configEnvironment } from '../config';
 
 export const useEnvironmentStore = defineStore('environment', () => {
-  const baseUrl = ref('/');
-  const environment = ref<EnvironmentType>(typeof ENVIRONMENT !== 'undefined' ? ENVIRONMENT : PRODUCTION_ENV);
-  const version = ref('');
-  const isMockerMode = ref(false);
+  const baseUrl = ref(configEnvironment.getBaseUrl());
+  const environment = ref<EnvironmentType>(configEnvironment.getEnvironment());
+  const version = ref(configEnvironment.getVersion());
+  const isMockerMode = ref(configEnvironment.getIsMockerMode());
 
-  const isDev = computed(() => environment.value === DEVELOPMENT_ENV);
-  const isStage = computed(() => environment.value === STAGING_ENV);
-  const isProduction = computed(() => environment.value === PRODUCTION_ENV);
+  const isDev = computed(() => environment.value === EnvironmentType.DEVELOPMENT);
+  const isStage = computed(() => environment.value === EnvironmentType.STAGING);
+  const isProduction = computed(() => environment.value === EnvironmentType.PRODUCTION);
 
   return {
     baseUrl,
