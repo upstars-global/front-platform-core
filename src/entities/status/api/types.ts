8915595d232b37cba42774c208a5f705e2f6e90d
Oleksandr Vozniuk <o.vozniuk@upstars.com>
2025-09-11 18:20:48 +0300

    chore(FP-3707): replace `Gift` with `DepositBonus` reward type and update related utilities
    - Update `Rewards` type definitions to include `DepositBonus` and refine field structures.
    - Adjust `getRewardGifts` and `getRewardGiftTitle` utilities to use the new reward type.

--- src/entities/status/api/types.ts
+++ src/entities/status/api/types.ts
@@ -1,24 +1,41 @@
+import { Currency } from '../../../shared/api';
+
 export enum RewardType {
   Cashback = 'cashback',
-  Gift = 'gift',
-  Freespin = 'freespin',
+  DepositBonus = 'deposit_bonus',
+  Freespin = 'free_spins',
   Cash = 'cash',
 }
 
 export type RewardsTypeCashbackValue = {
   title: string;
-  cashbackPercent: number;
+  bonus: number;
   wager: number;
+  winLimit: number;
 };
-export type RewardsTypeGiftValue = {
+export type RewardsTypeDepositBonusValue = {
   title: string;
   expired: number;
   description: string;
 };
-
+export type RewardsTypeFreeSpinsValue = {
+  title: string;
+  bonus: number;
+  bet: number;
+  wager: number;
+  winLimit: number;
+  currency: Currency
+};
+export type RewardsTypeCashValue = {
+  title: string;
+  bonus: number;
+  winLimit: number;
+  wager: number;
+  currency: Currency
+};
 export type Rewards = {
   type: RewardType;
-  value: RewardsTypeCashbackValue | RewardsTypeGiftValue;
+  value: RewardsTypeCashbackValue | RewardsTypeDepositBonusValue | RewardsTypeFreeSpinsValue | RewardsTypeCashValue;
 };
 export type StaticLevelDataResources = {
   id: string;
