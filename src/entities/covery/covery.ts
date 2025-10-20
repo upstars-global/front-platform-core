4c3b1bb05a1175a288ac30ee3823767e9144f8fd
Daniil Balianytsia <d.balianytsia@upstars.com>
2025-10-20 16:45:26 +0300

    feat(FP-3982): added fingerprintHelper and client logger BREAKING CHANGE: IRegisterDTO renamed to RegisterDTO

---
+++ src/entities/covery/covery.ts
@@ -0,0 +1,33 @@
+declare global {
+    interface Window {
+        antifrodScriptsPath?: string;
+        dfpObj?: {
+            getDFP: () => string;
+        };
+    }
+}
+
+export function addCoveryScript() {
+    if (typeof window !== "object" || typeof document !== "object") {
+        return;
+    }
+
+    const antifrodScriptsPath = window.antifrodScriptsPath || "/svc/af/assets/js/";
+
+    const coveryScript = document.createElement("script");
+    
+    coveryScript.lang = "javascript";
+    coveryScript.type = "text/javascript";
+    coveryScript.async = true;
+    coveryScript.src = `${antifrodScriptsPath}c-dfp.js`;
+
+    document.head.appendChild(coveryScript);
+}
+
+export function deviceFingerprint() {
+    if (typeof window !== "object" || typeof window.dfpObj !== "object") {
+        return "";
+    }
+
+    return window.dfpObj.getDFP() || "";
+}
