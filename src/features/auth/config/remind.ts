91b02374a09ffd37c8b6804ea8d71287b8ab62ec
Daniil Balianytsia <d.balianytsia@upstars.com>
2025-12-04 16:35:48 +0200

    feat(FP-4230): fixed path

--- src/features/auth/config/remind.ts
+++ src/features/auth/config/remind.ts
@@ -1,5 +1,5 @@
 import { createPromiseHook } from '../../../shared/helpers/config/createPromiseHook';
-import type { IUserRestorePasswordResource } from 'src/entities/user';
+import type { IUserRestorePasswordResource } from '../../../entities/user/api/types';
 
 export const afterRestorePasswordRequestHook = createPromiseHook<IUserRestorePasswordResource>({
   hookError: 'AFTER_RESTORE_PASSWORD_REQUEST_HOOK_ERROR',
