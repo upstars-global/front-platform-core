e576d9f49a006ff23233b39ae1051794ed75c6f0
Daniil Balianytsia <d.balianytsia@upstars.com>
2026-04-23 16:03:30 +0300

    feat(FP-5092): deposit-insurance

---
+++ src/entities/deposit-insurance/api/types.ts
@@ -0,0 +1,38 @@
+export enum DIErrorCode {
+  BALANCE_LOW = 'BALANCE_LOW',
+  DI_LIMIT = 'DI_LIMIT',
+  EMAIL_NOT_VERIFIED = 'EMAIL_NOT_VERIFIED',
+  DEPOSITS_NOT_FOUND = 'DEPOSITS_NOT_FOUND',
+  KYC_NOT_VERIFIED = 'KYC_NOT_VERIFIED',
+  DI_DISABLED = 'DI_DISABLED',
+  PAYOUT_EXISTS = 'PAYOUT_EXISTS',
+}
+
+export interface DIInfoResource {
+  isEnabled: boolean;
+  userActivationsLimit: number;
+  todayActivationsLeft: number;
+  todayInsuredSum: number;
+  userDepositPercent: number;
+  userMinBalance: number;
+  lastInsuranceDatetime: string | null;
+  giftWinLimit: number;
+  giftWager: number;
+}
+
+export interface DICreateSuccessResource {
+  id: string;
+}
+
+export interface DIValidationError {
+  field: string;
+  code: DIErrorCode;
+  template: string;
+  params: unknown[];
+}
+
+export interface DICreateErrorResource {
+  type: 'VALIDATION';
+  description: 'Validation error';
+  validationErrors: DIValidationError[];
+}
